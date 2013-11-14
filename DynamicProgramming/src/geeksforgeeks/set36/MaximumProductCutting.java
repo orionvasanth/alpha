75473ec57d57d8e6f2cbfcd9db5ec7a5b09d6eab
Vasanth Kumar Vadivelu <orionvasanth@gmail.com>
2013-11-14 11:49:51 +0530

    sync

--- DynamicProgramming/src/geeksforgeeks/set36/MaximumProductCutting.java
+++ DynamicProgramming/src/geeksforgeeks/set36/MaximumProductCutting.java
@@ -9,24 +9,27 @@ public class MaximumProductCutting {
 	 */
 	public static void main(String[] args) {
 		MaximumProductCutting cutting = new MaximumProductCutting();
-		System.out.println(cutting.findMaxProduct(10));
+		System.out.println(cutting.findMaxProduct(2));
 	}
-	
-	private int[] dp;
-	
-	public int findMaxProduct(int length){
-		dp = new int[length + 1];
-		Arrays.fill(dp, 1);
-		dp[2] = 1;
-		
-		return findMax(10);
+
+	private int[] product;
+
+	public int findMaxProduct(int length) {
+		product = new int[length + 1];
+		Arrays.fill(product, Integer.MIN_VALUE);
+		product[1] = 1;
+		product[2] = 1;
+		return findMax(length);
 	}
-	
-	private int findMax(int length){
-		for(int i = 1; i < length; i++){
-			dp[length] = Math.max(i * (length - i), findMax(length-i) * i);
+
+	private int findMax(int length) {
+		if(product[length] != Integer.MIN_VALUE)
+			return product[length];
+		for (int i = 1; i < length; i++) {
+			product[length] = Math.max(product[length], i * (length - i));
+			product[length] = Math.max(product[length], product[i] * findMax(length - i));
 		}
-		return dp[length];
+		return product[length];
 	}
 
 }
